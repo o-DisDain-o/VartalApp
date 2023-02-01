b92dff83c377680b9b3bb78408b6d467289a59cd
o-DisDain-o <78275204+o-DisDain-o@users.noreply.github.com>
2023-02-01 13:24:00 +0530

    Add message chats prototype

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Message() {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+      <div className="messageInfo">
+        <img src="https://alliancensut.com/wp-content/uploads/photo-gallery/Devang.jpeg?bwg=1661086493" alt="Profile Picture" />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img src="https://alliancensut.com/wp-content/uploads/photo-gallery/Devang.jpeg?bwg=1661086493" alt="Profile Picture" />
+      </div>
+    </div>
   )
 }
 
